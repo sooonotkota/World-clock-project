04886b316990d8161420d5947072f569340a7d1a
sooonotkota <dakotart08@icloud.com>
2024-06-11 10:43:44 -0500

    adding javascript properties

---
+++ index.js
@@ -0,0 +1,5 @@
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = document.querySelector("#los-angeles . date");
+losAngelesTimeElement = losAngelesElement.querySelector(".time");
+losAngelesDateElement.innerHTML = "June 6th, 2024";
+losAngelesTimeElement.innerHTML = "8:40:10 <small>PM</small";
